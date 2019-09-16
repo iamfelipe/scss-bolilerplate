4ace3c8af0f53040ee1a09d3c1999167e1620161
Felipe Castillo <fcastillo@serempre.com>
2019-09-16 12:28:21 -0500

    Webpack config compact fixed.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,10 @@
-import bsCustomFileInput from 'bs-custom-file-input'
+import bsCustomFileInput from "bs-custom-file-input";
 
-$(document).ready(function () {
-  bsCustomFileInput.init()
-  $('select').selectpicker({
-    style: '',
-    styleBase: 'form-control',
+$(document).ready(function() {
+  bsCustomFileInput.init();
+  $("select").selectpicker({
+    style: "",
+    styleBase: "form-control"
     // liveSearch: true
-  })
-})
+  });
+});
