95b77bcc920d2a14579d53c33270e9e9b491f38b
Felipe Castillo <fcastillo@serempre.com>
2019-09-26 12:17:59 -0500

    Webpack improved.

--- webpack/config.development.js
+++ webpack/config.development.js
@@ -1,13 +1,12 @@
 // You can add other webpack configuration (plugins, loaders, etc).
 // Apart from ES6 Import/Export, Gulp was able to do all my other work so this file is mainly empty.
 const entry = require("./entry");
+const output = require("./output");
 
 module.exports = {
   entry,
   mode: "development",
   devtool: "inline-cheap-source-map",
   loader: { test: /\.js$/, loader: "babel", query: { compact: false } },
-  output: {
-    filename: "[name].js"
-  }
+  output
 };
