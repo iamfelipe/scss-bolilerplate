03678d19b92ce36af5c6c87690acbe91b19e1c80
Felipe Castillo <fcastillo@serempre.com>
2020-03-04 14:03:47 -0500

    Navbar custom styles.

--- src/js/index.ts
+++ src/js/index.ts
@@ -4,12 +4,14 @@
 //   let select = new bsCustomFileInput($("select"));
 // });
 
-let myNames = [];
-myNames = ["26", 1, true, {}.toString()];
+console.log($("body"));
 
-for (const name of myNames) {
-  console.log(name);
-}
-
-const myAge = 26.23;
-console.log(myAge);
+// let myNames = [];
+// myNames = ["26", 1, true, {}.toString()];
+//
+// for (const name of myNames) {
+//   console.log(name);
+// }
+//
+// const myAge = 26.23;
+// console.log(myAge);
