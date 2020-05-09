950513de40beef6f6c817620d7927ec130140f8e
Felipe Castillo <fcastillo@serempre.com>
2020-05-09 16:26:11 -0500

    Clean assets con start.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,11 +1,22 @@
 // Node modules
 const merge = require("webpack-merge");
 
+// Webpack plugins
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 // Config files
 const common = require("./webpack.common.js");
+
+// Configure Clean webpack
+const configureCleanWebpack = () => {
+  return {
+    cleanStaleWebpackAssets: false,
+  };
+};
 
 // Development module exports
 module.exports = merge(common, {
   mode: "development",
   devtool: "inline-source-map",
+  plugins: [new CleanWebpackPlugin(configureCleanWebpack())],
 });
