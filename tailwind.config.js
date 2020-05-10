85d262debbef0579b16534b05136b3474a902536
Felipe Castillo <fcastillo@serempre.com>
2020-05-09 23:05:33 -0500

    Theme colors setup.

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,29 @@ module.exports = {
     extend: {
       colors: {
         black: "var(--color-black)",
+
+        primary: {
+          100: "var(--color-primary-100)",
+          200: "var(--color-primary-200)",
+          300: "var(--color-primary-300)",
+          400: "var(--color-primary-400)",
+          500: "var(--color-primary-500)",
+          600: "var(--color-primary-600)",
+          700: "var(--color-primary-700)",
+          800: "var(--color-primary-800)",
+          900: "var(--color-primary-900)",
+        },
+        secondary: {
+          100: "var(--color-secondary-100)",
+          200: "var(--color-secondary-200)",
+          300: "var(--color-secondary-300)",
+          400: "var(--color-secondary-400)",
+          500: "var(--color-secondary-500)",
+          600: "var(--color-secondary-600)",
+          700: "var(--color-secondary-700)",
+          800: "var(--color-secondary-800)",
+          900: "var(--color-secondary-900)",
+        },
       },
       fontSize: {
         xs: "var(--text-xs)",
