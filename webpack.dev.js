e5982e96478400fa5ca1e42f4946bba08e67589d
Felipe Castillo <fcastillo@serempre.com>
2020-05-13 09:09:11 -0500

    Webpack modes refactor.

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,30 @@
 // Node modules
 const merge = require("webpack-merge");
 
+// Webpack plugins
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 // Config files
-const common = require("./webpack.common.js");
+const commonConfig = require("./webpack.common.js");
+const settings = require("./webpack.settings.js");
+
+// Configure Clean webpack
+const configureCleanWebpack = () => {
+  return {};
+};
+
+// Configure the Postcss loader
+const configurePostcssLoader = () => {
+  return {
+    test: /\.s[ac]ss$/i,
+    use: [
+      {
+        loader: "style-loader",
+      },
+    ],
+  };
+};
 
 // Configure Image loader
 const configureImageLoader = () => {
@@ -19,11 +41,35 @@ const configureImageLoader = () => {
   };
 };
 
-// Development module exports
-module.exports = merge.smart(common, {
+const regex = new RegExp(/development/);
+const development = process.argv.some((e) => regex.test(e));
+
+const devConfig = {
   mode: "development",
   devtool: "inline-source-map",
-  module: {
-    rules: [configureImageLoader()],
+  output: {
+    publicPath: settings.devServerConfig.public(),
   },
-});
+  devServer: {
+    compress: true,
+    contentBase: settings.paths.templates,
+    disableHostCheck: development,
+    host: settings.devServerConfig.host(),
+    hot: true,
+    port: settings.devServerConfig.port(),
+    watchContentBase: true,
+    writeToDisk: true,
+  },
+  module: {
+    rules: [configurePostcssLoader(), configureImageLoader()],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(configureCleanWebpack()),
+  ],
+};
+
+// Development module exports
+module.exports = merge.strategy({
+  "module.rules": "prepend",
+})(commonConfig, devConfig);
